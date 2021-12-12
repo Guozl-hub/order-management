1e4fd55ba25413ec986877591deb4b0f4aad786e
ZHAOLGU <zhaolong.guo@thoughtworks.com>
2021-12-12 21:49:51 +0800

    [zhaolong guo]feat:add unit test for HeartBeatApi.

--- src/test/java/com/example/ordermanagement/api/HeartBeatApiTest.java
+++ src/test/java/com/example/ordermanagement/api/HeartBeatApiTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example.ordermanagement.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HeartBeatApi.class)
 class HeartBeatApiTest {
-  
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void should_return_ok() throws Exception {
+        mockMvc.perform(post("/diagnostic/heartbeat"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Ok"));
+    }
 }
